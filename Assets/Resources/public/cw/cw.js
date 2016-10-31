133e851f9d68b3370ac183238ef4de67d94178d5
EchoWine <dimitri.who@gmail.com>
2016-10-31 15:15:42 +0100

    Added "sorting of contents" in dashboard; Added links in calendar

---
+++ Assets/Resources/public/cw/cw.js
@@ -0,0 +1,54 @@
+var cw = {};
+
+cw.load = function(){
+
+	var d = $('[cw-default]');
+
+	if(d)
+		cw.click(d);
+};
+
+cw.click = function(element){
+
+	var object = element.attr('cw-show');
+	var parts = object.split(":");
+	var container = $("[cw-container='"+parts[0]+"']");
+	var elements = parts[1].split("|");
+
+	if(container)
+		cw.show(container,elements);
+};
+
+cw.show = function(container,elements){
+	$.map(container.find('[cw-element]'),function(val){
+		val = $(val);
+		type = val.attr('cw-element');
+		types = type.split("|");
+
+		var show = false;
+
+
+
+		for(i = 0;i < types.length;i++){
+			type = types[i];
+			if(elements.indexOf(type) != -1){
+				show = true;
+				break;
+			}
+		}
+
+		if(show)
+			val.show();
+		else
+			val.hide();
+
+	});
+};
+
+$('body').on('click','[cw-show]',function(){
+	cw.click($(this));
+});
+
+$(document).ready(function(){
+	cw.load();
+});
