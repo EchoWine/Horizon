6c36ba3cefb7b0ce8e5720f0bd2d8fbc3ec93ce9
EchoWine <dimitri.who@gmail.com>
2016-12-10 14:26:44 +0100

    Cleanup; Added IndexController

---
+++ WT/Controller/Admin/IndexController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace WT\Controller\Admin;
+
+use CoreWine\Http\Router;
+use CoreWine\Http\Request;
+use Auth\Service\Auth;
+
+use CoreWine\Http\Controller as Controller;
+
+class IndexController extends Controller{
+
+	/**
+	 * Middleware
+	 *
+	 * @var Array
+	 */
+	public $middleware = ['Admin\Middleware\Authenticate'];
+
+	/**
+	 * Define your routes
+	 *
+	 * @param Router $router
+	 */
+	public function __routes($router){
+
+		$router -> any('/','index') -> as('index');
+	}
+	
+	/**
+	 * @ANY
+	 *
+	 * @return Response
+	 */
+	public function index(){
+
+		return $this -> redirect('admin/dashboard');
+	}
+}
+
+?>
