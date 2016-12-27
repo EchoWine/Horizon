040164f8acba1d2715ceedbe7df24e56a71d9334
EchoWine <dimitri.who@gmail.com>
2016-12-27 22:03:11 +0100

    Imporoved reader

--- WT/Resources/public/assets/wt/reader.js
+++ WT/Resources/public/assets/wt/reader.js
@@ -1,29 +1,89 @@
 WT.reader = {};
 
+WT.reader.page = 1;
+
 WT.reader.mode = function(mode){
 	$.cookie("wt.reader.mode",mode);
-	$('.wt-manga-reader-container').attr('wt-manga-reader-container-mode',mode);
+	$('.wt-manga-reader-mode').attr('wt-manga-reader-container-mode',mode);
 };
+
+
+WT.reader.first = function(){
+	if(!window.location.hash || window.location.hash == '#'){
+		window.location.hash = "#0";
+		WT.reader.page = 0;
+	}
+};
+
+WT.reader.updateSelect = function(){
+	$('.wt-manga-reader-select').val(parseInt(window.location.hash.replace("#","")));
+};
+
+WT.reader.prev = function(){
+
+	var page = parseInt(window.location.hash.replace("#","")) - 1;
+
+	if(page >= 0)
+		window.location.hash = '#'+page;
+
+	WT.reader.updateSelect();
+
+};
+
+$('.wt-manga-reader-select').on('change',function(){
+	window.location.hash = '#'+$(this).val();
+});
+
+WT.reader.next = function(){
+
+	var page = parseInt(window.location.hash.replace("#","")) + 1;
+
+	if(page <= $('.wt-manga-reader-all.wt-manga-reader').find('img').length)
+		window.location.hash = '#'+page;
+
+	WT.reader.updateSelect();
+};
+
 
 
 $("[name='wt-reader-mode']").on('change',function(){
 	WT.reader.mode($(this).val());
 });
 
+$('.wt-manga-reader-prev').on('click',function(){
+	
+	WT.reader.prev();
+});
 
-WT.reader.first = function(){
-	if(!window.location.hash || window.location.hash == '#'){
-		window.location.hash = "#1";
+$('.wt-manga-reader-next').on('click',function(){
+
+	WT.reader.next();
+});
+
+$('body').keydown(function(e){
+	var code = e.keyCode || e.which;
+	console.log(code);
+	switch(code){
+
+		case 37:
+			WT.reader.prev();
+		break;
+		case 39:
+			WT.reader.next();
+		break;
 	}
-};
+});
 
 $(document).ready(function(){
 	var mode = $.cookie('wt.reader.mode');
 
 	mode = mode ? mode : 'all';
-	
+
 	$("[name='wt-reader-mode']").val(mode);
 	WT.reader.mode(mode);
 	WT.reader.first();
 });
 
+$('.wt-reader-chapters').on('change',function(){
+	window.location.href = $(this).val();
+})
