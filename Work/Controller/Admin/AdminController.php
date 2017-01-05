9e85bd59f8f967031fb023510d756e1acfb91a2e
EchoWine <dimitri.who@gmail.com>
2017-01-06 00:08:10 +0100

    Added work

---
+++ Work/Controller/Admin/AdminController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Work\Controller\Admin;
+
+use Admin\Controller\AdminController as AdminBasicController;
+
+abstract class AdminController extends AdminBasicController{
+
+	public $view = 'WT/admin/item';
+}
+
+?>
---
+++ Work/Model/Customer.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Work\Model;
+
+use CoreWine\DataBase\ORM\Model;
+
+class Customer extends Model{
+
+	/**
+	 * Table name
+	 *
+	 * @var
+	 */
+	public static $table = 'wk_customers';
+
+	/**
+	 * Set schema fields
+	 *
+	 * @param Schema $schema
+	 */
+	public static function fields($schema){
+
+		$schema -> id();
+
+		$schema -> string('fullname');
+
+		$schema -> string('address');
+
+		$schema -> string('vat');
+
+		$schema -> string('tax_code');
+
+		$schema -> string('notes');
+		
+		$schema -> toOne(\Auth\Model\User::class,'user');
+
+	}
+}
+
+?>
